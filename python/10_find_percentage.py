ffe91fef6cdead49eace4bb36890e2673b1aa08b
Julio Formiga <julio.formiga@gmail.com>
2023-09-18 09:35:39 +0200

    Find the percentage

---
+++ python/10_find_percentage.py
@@ -0,0 +1,16 @@
+# n = int(input())
+# student_marks = {}
+# for _ in range(n):
+#     name, *line = input().split()
+#     scores = list(map(float, line))
+#     student_marks[name] = scores
+# query_name = input()
+student_marks = {
+    'Krishna': [67, 68, 69],
+    'Arjun': [70, 98, 63],
+    'Malika': [52, 56, 60],
+}
+query_name = 'Malika'
+
+soma = sum([s for s in student_marks[query_name]])
+print(f'{(soma/3):.2f}')
